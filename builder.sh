b1b0165ac22187755c5405b9da7a67062ccb3fe9
julia <julia@volcanocoffee.monster>
2023-04-05 18:07:47 +0800

    added builder

---
+++ builder.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+ARCH="${ARCH:-x86_64}"
+
+addCoprs(){
+    curl ostree remote add copr-solopasha-hyprland https://download.copr.fedorainfracloud.org/results/solopasha/hyprland/fedora-${RELEASE}-${ARCH}/
+}
+
+installPkgs(){
+    rpm-ostree install -y  \
+    xorg-x11-server-Xwayland \
+    gamescope \
+    xdg-desktop-portal \
+    polkit-gnome \
+    mako \ 
+    python3 \
+    python3-pip \
+    waybar \ 
+    kitty \
+    wlsunset \ 
+    swaylock \
+    swayidle \
+    swaybg \
+    wofi \
+    hyprland \
+    sddm
+}
+
+addFlatpakRemotes(){
+    # fedora oci
+    printf "adding fedora oci to flatpak if it does not exists\n"
+    flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+    
+    # flathub main
+    printf "adding flathub to flatpak if it does not exists\n"
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    
+    # flathub-beta
+    printf "adding flathub beta to flatpak if it does not exists\n"
+    flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+
+    # kdeapps 
+    printf "adding kdeapps to flatpak if it does not exists\n"
+    flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
+
+    # gnome-nightly
+    printf "adding gnome-nightly to flatpak if it does not exists\n"
+    flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+
+    # update appstream
+    printf "updating flatpak appstream\n"
+    flatpak update --appstream
+}
+
+setProfile(){
+    printf 'export SDL_VIDEODRIVER=wayland\n' | tee -a /etc/profile
+    printf 'export _JAVA_AWT_WM_NONREPARENTING=1\n' | tee -a /etc/profile
+    printf 'export QT_QPA_PLATFORM=wayland\n' | tee -a /etc/profile
+    printf 'export XDG_CURRENT_DESKTOP=sway\n' | tee -a /etc/profile
+    printf 'export XDG_SESSION_DESKTOP=sway\n' | tee -a /etc/profile
+    printf 'export GDK_BACKEND="wayland,x11"\n' | tee -a /etc/profile
+    printf 'export MOZ_ENABLE_WAYLAND=1\n' | tee -a /etc/profile
+}
+
+main(){
+    # coprs   
+    addCoprs
+    #
+    addFlatpakRemotes
+    #
+    installPkgs
+    #
+    setProfile
+}
+
+if [ -n "$1" ]; then
+    $1
+else
+    main
+fi
