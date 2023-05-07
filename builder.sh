84fde1346abcd72aa563ed3db7f9ea7da478330b
julia <julia@volcanocoffee.monster>
2023-05-08 07:55:36 +0800

    updated aliases

--- builder.sh
+++ builder.sh
@@ -36,54 +36,29 @@ rpm-ostree install -y \
   pipewire-alsa \
   pipewire-jack-audio-connection-kit \
   pipewire-plugin-libcamera \
-  just
+  just \
+  stow \
+  git
 
-  
 rpm-ostree install -y \
   xdg-desktop-portal-hyprland \
   waybar-hyprland \
   hyprland
 
-# fedora
-printf "adding fedora oci to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
-
-# fedora testing
-printf "adding fedora oci testing to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists fedora-testing oci+https://registry.fedoraproject.org#testing
-
-# flathub main
-printf "adding flathub to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  
-# flathub-beta
-printf "adding flathub beta to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-
-# kdeapps 
-printf "adding kdeapps to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists kdeapps-nightly --from https://distribute.kde.org/kdeapps.flatpakrepo
-
-# gnome-nightly
-printf "adding gnome-nightly to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
-
-# elemenetoryos-appcenter
-printf "adding elemenetoryos-appcenter to flatpak if it does not exists\n"
-flatpak remote-add --if-not-exists elemenetoryos-appcenter https://flatpak.elementary.io/repo.flatpakrepo
-
-# endless-os
-printf "adding endless os apps and sdk to flatpak if it does not exists\n"
 wget -O /tmp/eos-flatpak-keyring.gpg https://origin.ostree.endlessm.com/keys/eos-flatpak-keyring.gpg
+
+flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+flatpak remote-add --if-not-exists fedora-testing oci+https://registry.fedoraproject.org#testing
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+flatpak remote-add --if-not-exists kdeapps-nightly --from https://distribute.kde.org/kdeapps.flatpakrepo
+flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+flatpak remote-add --if-not-exists elemenetoryos-appcenter https://flatpak.elementary.io/repo.flatpakrepo
 flatpak remote-add --if-not-exists --gpg-import=/tmp/eos-flatpak-keyring.gpg endlessos-sdk https://ostree.endlessm.com/ostree/eos-sdk
 flatpak remote-add --if-not-exists --gpg-import=/tmp/eos-flatpak-keyring.gpg endlessos-apps https://ostree.endlessm.com/ostree/eos-apps
 
-# set profile
-#touch /etc/profile
-#chmod 644 /etc/profile
-printf 'alias /usr/bin/sh /usr/bin/dash\n' | tee -a /etc/profile
-printf 'alias /bin/sh /usr/bin/dash\n' | tee -a /etc/profile
-printf 'alias sh /usr/bin/dash\n' | tee -a /etc/profile
+printf 'alias "docker"="/usr/bin/podman"\n' | tee -a /etc/profile
+printf 'alias "sh"="/usr/bin/dash"\n' | tee -a /etc/profile
 printf 'export SDL_VIDEODRIVER=wayland\n' | tee -a /etc/profile
 printf 'export _JAVA_AWT_WM_NONREPARENTING=1\n' | tee -a /etc/profile
 printf 'export QT_QPA_PLATFORM=wayland\n' | tee -a /etc/profile
