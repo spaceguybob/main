1fd96f3287deda5d4d16086d32f2f2c2dc75247e
julia <julia@volcanocoffee.monster>
2023-06-18 06:37:18 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -4,9 +4,12 @@ set -ouex pipefail
 # sudo -> doas + doas-sudo-shim
 rm /etc/dnf/protected.d/sudo.conf
 rpm-ostree override remove \
-  sudo sudo-python-plugin
+  sudo \
+  sudo-python-plugin
+  
 rpm-ostree install -y \
   opendoas
+
 wget -O /usr/bin/sudo https://github.com/jirutka/doas-sudo-shim/raw/v0.1.1/sudo
 chmod +x /usr/bin/sudo
 touch /etc/dnf/protected.d/doas.conf
